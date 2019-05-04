4d2ab3de5a27deb7341a71eb0bba252f2493f6e1
Jonathan Sharman <jonathan.sharman@gmail.com>
2019-05-03 20:01:58 -0500

    Initial combat mechanics. Changes to camera and movement controls. Release.

---
+++ docs/resources.js
@@ -0,0 +1,3 @@
+﻿C$common$addTextRes('image_sheets.txt', "S,0,twocans_everything\nC,600,600\nT,t1.png,0,0,600,600,42\nI,0,0,600,600,bg.png");
+C$common$resourceManifest = "IMGSH,bg.png,,twocans_everything";
+C$common$jsFilePrefix = "";
